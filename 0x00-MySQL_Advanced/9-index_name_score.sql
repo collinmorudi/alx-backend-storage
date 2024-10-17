2e6307d30bc9b1c3bef970b39a317fbc46bc6d7b
Collin Morudi <collinmorudi@gmail.com>
2024-10-17 20:12:24 +0200

    Fix: fixed errors

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,11 +1,3 @@
--- commends will be added soon
-
-ALTER TABLE names ADD first_letter CHAR(1);
-
-UPDATE names SET first_letter = LEFT(name, 1);
-
-CREATE INDEX idx_name_first_score ON names (first_letter, score);
-
 -- index_name_score.sql
 ALTER TABLE names ADD first_letter CHAR(1);
 UPDATE names SET first_letter = LEFT(name, 1);
