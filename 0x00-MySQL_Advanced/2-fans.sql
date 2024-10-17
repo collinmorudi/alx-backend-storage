db8c6f2083bae0908a0083db0561d43191d6f779
Collin Morudi <collinmorudi@gmail.com>
2024-10-17 21:24:41 +0200

    Fix: fixed errors

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,13 +1,6 @@
 -- 2-fans.sql
 
--- Create a temporary table to store the results
-CREATE TEMPORARY TABLE IF NOT EXISTS band_fans AS
 SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
-GROUP BY origin;
-
--- Select the origins and their corresponding fan counts, ordered by nb_fans
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM band_fans
 GROUP BY origin
 ORDER BY nb_fans DESC;
