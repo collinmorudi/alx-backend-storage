e630f26c091fa47a38f30d7fdc8225583aacb11c
Collin Morudi <collinmorudi@gmail.com>
2024-10-17 21:06:47 +0200

    Fix: fixed errors

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,11 @@
--- Script to list Glam rock bands ranked by their longevity
+-- List all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name,
-       IFNULL(2022 - formed, 2022 - split) AS lifespan
-FROM metal_bands
-WHERE genre = 'Glam rock'
-ORDER BY lifespan DESC;
-
-SELECT band_name,
-       IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
-FROM metal_bands
-WHERE genre = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT 
+    band_name,
+    (2022 - formed) AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    style = 'Glam rock'
+ORDER BY 
+    lifespan DESC;
