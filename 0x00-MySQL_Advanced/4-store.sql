391bf0a3d11541f8619d9d224d3d3ffef61e372e
Collin Morudi <collinmorudi@gmail.com>
2024-10-17 15:13:21 +0200

    Add: store.sql

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,44 @@
+-- Script to create a trigger that decreases the quantity of an item
+-- after adding a new order
+
+-- Initial setup
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE IF NOT EXISTS items (
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL DEFAULT 10
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    item_name VARCHAR(255) NOT NULL,
+    number INT NOT NULL
+);
+
+INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+
+-- Create the trigger
+DELIMITER //
+
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END //
+
+DELIMITER ;
+
+-- Test the trigger
+SELECT * FROM items;
+SELECT * FROM orders;
+
+INSERT INTO orders (item_name, number) VALUES ('apple', 1);
+INSERT INTO orders (item_name, number) VALUES ('apple', 3);
+INSERT INTO orders (item_name, number) VALUES ('pear', 2);
+
+SELECT "--";
+SELECT * FROM items;
+SELECT * FROM orders;
