2d4e45597dcf8258acebd733f28f710510875d18
Collin Morudi <collinmorudi@gmail.com>
2024-10-17 20:28:37 +0200

    Fix: fixed errors

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,8 +2,8 @@
 -- Then group the results by origin and sum the number of fans per origin
 -- Finally, order the results by the total number of fans in descending order
 
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(nb_fans) AS total_fans, 
+       RANK() OVER (ORDER BY SUM(nb_fans) DESC) AS rank
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans DESC;
-
+ORDER BY rank;
