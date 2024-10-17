9a13c1980903aa291e7c7098eaa1f1d66aa0f7a3
Collin Morudi <collinmorudi@gmail.com>
2024-10-17 21:35:20 +0200

    Fix: fixed errors

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- 3-glam_rock.sql
 
-SELECT band_name, 
-       IFNULL(2022 - formed, 2022 - split) AS lifespan
-FROM metal_bands
-WHERE genre = 'Glam rock'
+SELECT b.band_name,
+       IFNULL(2022 - b.formed, 2022 - b.split) AS lifespan
+FROM metal_bands b
+WHERE b.genre = 'Glam rock'
 ORDER BY lifespan DESC;
