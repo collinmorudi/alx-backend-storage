1d66845d64d986ca7bd22885f93d15a2f21f2461
Collin Morudi <collinmorudi@gmail.com>
2024-10-18 05:54:24 +0200

    Fix: fixed errors

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,5 @@
 SELECT b.band_name,
        IFNULL(2022 - b.formed, 2022 - b.split) AS lifespan
 FROM metal_bands b
-WHERE LIKE '%Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
